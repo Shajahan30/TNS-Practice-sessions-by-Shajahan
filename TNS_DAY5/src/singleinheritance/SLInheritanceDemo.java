55b9e395c5a2222820af7c1f8700124699410530
Shajahan Sayyed <shajahansayyedz@gmail.com>
2024-07-02 21:49:18 +0530

    Day5 commit

---
+++ TNS_DAY5/src/singleinheritance/SLInheritanceDemo.java
@@ -0,0 +1,13 @@
+package singleinheritance;
+
+public class SLInheritanceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student student = new Student("Shaan","TC0111","Pune",99999999L, 1001, "JSPM");
+		System.out.println(student);
+
+	}
+
+}
