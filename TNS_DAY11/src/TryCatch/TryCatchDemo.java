9b452dbc75b47c438e8d12f57a0ede3892fde41b
Shajahan Sayyed <shajahansayyedz@gmail.com>
2024-07-23 11:15:16 +0530

    day 11 commit

---
+++ TNS_DAY11/src/TryCatch/TryCatchDemo.java
@@ -0,0 +1,13 @@
+package TryCatch;
+
+public class TryCatchDemo {
+
+	public static void main(String[] args) {
+	System.out.println("I am in main method");
+	TryCatchExample.performDivision(12,3);
+	TryCatchExample.performDivision(12,0);
+	
+
+	}
+
+}
